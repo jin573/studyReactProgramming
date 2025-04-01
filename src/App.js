dcfc7c77eb848cfe77cd9243c126d041de8026ad
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-04-01 12:04:53 +0900

    Todo 컴포넌트 생성하고 JSX 특징 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
+  let title = "제목";
+  
+  let output = <div className='App'>
+  <Todo />
+  <Todo />
+  <h2>{title}</h2>
+  </div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    output
+
   );
 }
 
