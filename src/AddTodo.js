afbf63cd52cb5f7dd355ad6b98bc42a66930a173
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-04-08 11:27:07 +0900

    AddTodo 변수명 수정

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import { Button, Grid, TextField } from "@mui/material";
 
 const AddTodo = (props) =>{
-    const [item, setItem] = useState({title: ""});
+    const [inputItem, setItem] = useState({title: ""});
+
+    //onInputChange
+    const onInputChange = (e) => {
+        setItem({title: e.target.value});//입력된 값을 저장
+        console.log(inputItem);
+    };
 
     return(
         <Grid container style={{marginTop:20}}>
             <Grid xs={11} md={11} item style={{paddingRight:16}}>
-                <TextField placeholder='Add Todo here' fullWidth/>
+                <TextField placeholder='Add Todo here' fullWidth onChange={onInputChange} value={inputItem.title}/>
             </Grid>
             <Grid xs={1} md={1} item>
                 <Button fullWidth style={{height:'100%'}} color="secondary" variant="outlined">
