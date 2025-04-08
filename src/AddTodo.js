dd1a81f55dbed34a98b2ce21bcc456bf52a3cc96
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-04-08 11:46:18 +0900

    Todo Item 추가하기 (Enterkey로 추가하기)

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -5,6 +5,11 @@ const AddTodo = (props) =>{
     const [inputItem, setItem] = useState({title: ""});
     const addItem = props.addItem; // props로 addItem 가져옴
 
+    const enterKeyEventHandler = (e) =>{
+        if(e.key == 'Enter'){
+            onButtonClick();
+        }
+    }
     const onButtonClick = () => {
         addItem(inputItem);
         setItem({title: ""});
@@ -19,7 +24,7 @@ const AddTodo = (props) =>{
     return(
         <Grid container style={{marginTop:20}}>
             <Grid xs={11} md={11} item style={{paddingRight:16}}>
-                <TextField placeholder='Add Todo here' fullWidth onChange={onInputChange} value={inputItem.title}/>
+                <TextField placeholder='Add Todo here' fullWidth onChange={onInputChange} onKeyPress={enterKeyEventHandler} value={inputItem.title}/>
                 {/**value부분이 없으면 state만 변경되고 TextField 컴포넌트의 value 값은 변경되지 않음 */}
             </Grid>
             <Grid xs={1} md={1} item>
