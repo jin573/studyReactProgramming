c8f3abdf612e678cccec282208cebac575ebb7a0
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-04-08 10:47:48 +0900

    todo ui 적용 (margin 오류 있음)

--- src/Todo.js
+++ src/Todo.js
@@ -1,21 +1,40 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import {ListItem, ListItemText, InputBase, Checkbox} from "@mui/material";
+
 
 const Todo = (props) => {
-    const[item, setItem] = useState(props.item);/*상태변경 함수 생성 */
+    const[item, setItem] = useState(props.item);/*상태변경 함수 생성. 부모가 넘겨준 item 객체를 item 상태 변수가 가리킴 */
 
     return (
-        <div className='Todo'>
-            {/*리터럴 타입*/
-            /*<input type='checkbox' id='todo0' name='todo0' value='todo0'/>*/}
-            <input
-                type='checkbox'
-                id={item.id}
-                name={item.id}
-                checked={item.done}
+        <ListItem>
+            <Checkbox checked={item.done}/>
+            <ListItemText>
+                <InputBase
+                    inputProps={{"aria-label": "naked"}}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
                 />
-            <label for={item.id}>{item.title} {props.id}
-            </label>
-        </div>
+            </ListItemText>
+        </ListItem>
+
+        
+        //     <div className='Todo'>
+        //     {/*리터럴 타입*/
+        //     /*<input type='checkbox' id='todo0' name='todo0' value='todo0'/>*/}
+        //     <input
+        //         type='checkbox'
+        //         id={item.id}
+        //         name={item.id}
+        //         checked={item.done}
+        //         />
+        //     <label for={item.id}>{item.title} {props.id}
+        //     </label>
+        // </div>
+        
     );
 
 };
