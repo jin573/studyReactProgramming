8fc5f56231a5d5a6b2cbca7529eaa42efb7547fa
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-05-13 00:49:08 +0900

    access token local storage로 관리하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Todo from './Todo';
-import React, { useEffect, useState } from 'react';
+import "./App.css";
+import Todo from "./Todo";
+import React, { useEffect, useState } from "react";
 import { Container, List, Paper } from "@mui/material";
 import AddTodo from "./AddTodo";
 //import { API_BASE_URL } from './api-config';
-import { call } from './service/ApiService';
+import { call } from "./service/ApiService";
 
 function App() {
-/*  const[items, setItems] = useState([
+  /*  const[items, setItems] = useState([
   {
     id:"0",
     title: "Hello World 1",
@@ -20,29 +20,23 @@ function App() {
   }]);*/
 
   //4. todoitem 삭제 기능 생성 후 useState 초기화
-  const[items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    call("/todo", "GET", null)
-    .then((response) => setItems(response.data));
+    call("/todo", "GET", null).then((response) => setItems(response.data));
   }, []);
 
   const addItem = (item) => {
-    call("/todo", "POST", item)
-    .then((response) => setItems(response.data));
+    call("/todo", "POST", item).then((response) => setItems(response.data));
   };
 
   const deleteItem = (item) => {
-    call("/todo", "DELETE", item)
-    .then((response) => setItems(response.data));
+    call("/todo", "DELETE", item).then((response) => setItems(response.data));
   };
 
   const editItem = (item) => {
-    call("/todo", "PUT", item)
-    .then((response) => setItems(response.data));
+    call("/todo", "PUT", item).then((response) => setItems(response.data));
   };
-
-
 
   // useEffect(() => {
   //   const requestOptions = {
@@ -61,42 +55,46 @@ function App() {
   //     .then((response) => { setItems(response.data); } )
   //     .catch( (e) => {}); //자주 쓰이는 형태이다.
   // }, []);
-  
 
-// //AddTodo 에 넘겨줄 addItem 함수 생성
-//   const addItem=(item) => {
-//     item.id = "ID-" + items.length; //key를 위한 id
-//     item.done = false; //done 으로 초기화
-//     setItems([...items, item]); //items 배열을 spread 하고 item 추가
-//     console.log("items: ", items);
-//   };
+  // //AddTodo 에 넘겨줄 addItem 함수 생성
+  //   const addItem=(item) => {
+  //     item.id = "ID-" + items.length; //key를 위한 id
+  //     item.done = false; //done 으로 초기화
+  //     setItems([...items, item]); //items 배열을 spread 하고 item 추가
+  //     console.log("items: ", items);
+  //   };
 
-//   //Todo 컴포넌트를 만드는 곳에 넘겨줌 -> let todoItems
-//   const deleteItem=(item) =>{
-//     const newItems = items.filter(e=> e.id !== item.id); // 클릭된 item 을 제외한 items들이 newItems로 들어가 다시 배치된다. 
-//     setItems([...newItems]);
-//   };
+  //   //Todo 컴포넌트를 만드는 곳에 넘겨줌 -> let todoItems
+  //   const deleteItem=(item) =>{
+  //     const newItems = items.filter(e=> e.id !== item.id); // 클릭된 item 을 제외한 items들이 newItems로 들어가 다시 배치된다.
+  //     setItems([...newItems]);
+  //   };
 
-// //수정 기능 추가
-//   const editItem = () =>{
-//     setItems([...items]);
-//   }
+  // //수정 기능 추가
+  //   const editItem = () =>{
+  //     setItems([...items]);
+  //   }
 
-  let todoItems = 
-    items.length > 0 && (
-    <Paper style={{margin: 16}}>
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
       <List>
-        {items.map((item)=>(
-          <Todo item={item} key={item.id} deleteItem={deleteItem} editItem={editItem}/> //delete, edit 함수를 추가한다
+        {items.map((item) => (
+          <Todo
+            item={item}
+            key={item.id}
+            deleteItem={deleteItem}
+            editItem={editItem}
+          /> //delete, edit 함수를 추가한다
         ))}
       </List>
     </Paper>
-    );
+  );
 
   return (
-    <div className='App'>
+    <div className="App">
       <Container maxWidth="md">
-        <AddTodo addItem={addItem}/> {/**addItem 속성에 addItem 함수 넣어서 전달 */}
+        <AddTodo addItem={addItem} />{" "}
+        {/**addItem 속성에 addItem 함수 넣어서 전달 */}
         <div className="TodoList">{todoItems}</div>
       </Container>
     </div>
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,17 @@
 import { API_BASE_URL } from '../api-config';
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type" : "application/json",
+    });
+
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken!=null){
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type" : "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -36,7 +43,8 @@ export function signin(userDTO){
         // console.log("response : ", response);
         // alert("로그인 토큰: " + response.token);
         if(response.token){
-            window.location.href="/";
+            localStorage.setItem("ACCESS_TOKEN", response.token); //토큰 저장
+            window.location.href="/"; //토큰이 존재하는 경우 Todo 화면으로 리다이렉트
         }
     });
 }
