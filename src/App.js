986b8a6a17cc4f9fad180c1442050679e140f67c
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-05-13 00:59:56 +0900

    nav bar 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Todo from "./Todo";
 import React, { useEffect, useState } from "react";
-import { Container, List, Paper } from "@mui/material";
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography } from "@mui/material";
 import AddTodo from "./AddTodo";
 //import { API_BASE_URL } from './api-config';
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 function App() {
   /*  const[items, setItems] = useState([
@@ -90,8 +90,24 @@ function App() {
     </Paper>
   );
 
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>로그아웃</Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
   return (
     <div className="App">
+      {navigationBar}
       <Container maxWidth="md">
         <AddTodo addItem={addItem} />{" "}
         {/**addItem 속성에 addItem 함수 넣어서 전달 */}
