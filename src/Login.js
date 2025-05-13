109f6ee1a9f1b0842189c658c63cf12723098626
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-05-13 11:40:23 +0900

    회원가입 구현

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Grid, Typography, TextField, Button } from "@mui/material";
 import { signin } from "./service/ApiService";
-
+import {Link} from "react-router-dom";
 
 
 function Login(){
@@ -17,7 +17,7 @@ function Login(){
 
     return (
         <Container component="main" maxWidth="xs" style={{ marginTop: "8%"}}>
-            <Grid container spacint={2}>
+            <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <Typography component="h1" variant="h5">
                         로그인
@@ -32,12 +32,16 @@ function Login(){
                         <TextField variant='outlined' required fullWidth id="username" label="아이디" name='username' autoComplete='username'/>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField variant='outlined' required fullWidth id="password" label="패스워드" name='password' autoComplete='current-password'/>
+                        <TextField variant='outlined' required fullWidth id="password" label="패스워드" name='password' autoComplete='current-password' type="password"/>
                     </Grid>
                     <Grid item xs={12}>
                         <Button type="submit" fullWidth variant='contained' color="primary">
                             로그인
                         </Button>
+                    </Grid>
+                    <Grid item>
+                        <Link to="/signup" variant="body2">
+                        계정이 없습니까? 여기서 가입하세요.</Link>
                     </Grid>
                 </Grid>
             </form>
---
+++ src/SignUp.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {Container, Grid, Typography, TextField, Button} from "@mui/material";
+import { signup } from './service/ApiService';
+import {Link} from "react-router-dom";
+
+function SignUp(){
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.target); //폼에 있는 데이터를 얻기
+        const username = data.get("username");
+        const password = data.get("password");
+        signup({username: username, password: password}).then(
+            (response) => {
+                window.location.href = "/login";
+            }   
+        )
+    };
+
+
+return (
+    <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+        <form noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+                {/**계정 생성 */}
+                <Grid item xs={12}>
+                    <Typography component="h1" variant="h5">
+                        계정 생성
+                    </Typography>
+                </Grid>
+                {/** 아이디, 비밀번호 입력 */}
+                <Grid item xs={12}>
+                    <TextField
+                    autoComplete='fname'
+                    name="username"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id='username'
+                    label="아이디"
+                    autoFocus/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="패스워드"
+                    type="password"
+                    id='password'
+                    autoComplete='current-password'/>
+                </Grid>
+
+                {/** 계정 생성 */}
+                <Grid item xs={12}>
+                    <Button type="submit" fullWidth variant="contained" color="primary">
+                        계정 생성
+                    </Button>
+                </Grid>
+            </Grid>
+
+            {/**링크 */}
+            <Grid container justify="flex-end">
+                <Grid item>
+                    <Link to="/login" variant="body2">
+                    이미 계정이 있습니까? 로그인하세요.
+                    </Link>
+                </Grid>
+            </Grid>
+        </form>
+    </Container>
+);
+
+};
+
+export default SignUp;
